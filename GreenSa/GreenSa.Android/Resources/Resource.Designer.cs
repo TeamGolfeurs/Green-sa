8663367d968cb3ae4ecf0f53d3e33f3e91373cfe
Kibo4 <william.mocaer@gmail.com>
2018-02-06 19:46:43 +0100

    Mise à jour des diagrammes

--- GreenSa/GreenSa.Android/Resources/Resource.Designer.cs
+++ GreenSa/GreenSa.Android/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace GreenSa.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::GreenSa.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
