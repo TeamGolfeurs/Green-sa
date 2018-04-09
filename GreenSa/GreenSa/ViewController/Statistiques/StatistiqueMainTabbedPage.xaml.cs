2287d557cf1e038b2e03f8b7fc33f79ede6a3afd
Kibo4 <william.mocaer@gmail.com>
2018-04-09 20:13:53 +0200

    updating

--- GreenSa/GreenSa/ViewController/Statistiques/StatistiqueMainTabbedPage.xaml.cs
+++ GreenSa/GreenSa/ViewController/Statistiques/StatistiqueMainTabbedPage.xaml.cs
@@ -5,28 +5,45 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+
 
 namespace GreenSa.ViewController.Statistiques
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class StatistiqueMainTabbedPage : ContentPage
+    public partial class StatistiqueMainTabbedPage : TabbedPage
     {
         public StatistiqueMainTabbedPage()
         {
             InitializeComponent();
 
+
+           
+
         }
 
-        async protected override void OnAppearing()
+        protected async override void OnAppearing()
         {
-            //lire dans un fichier XML les noms des Classes statistiques
-            string assemblyName = "GreenSa.ViewController.Statistiques.SpecificStatistiques.DistanceClubPage";
-            Type t = Type.GetType(assemblyName);
-            ContentPage p = (ContentPage)Activator.CreateInstance(t);
-            await Navigation.PushAsync(p);
+            string path = "GreenSa.ViewController.Statistiques.SpecificStatistiques";
+            List < String > l = new List<String>();
+            l.Add("DistanceClubPage");
+            l.Add("ScoreEvolutionDetailsPage");
+            l.Add("ScoreEvolutionPage");
+
+
+            foreach(String file in l){
+                
+                string assemblyName = path+"."+file;
+                Type t = Type.GetType(assemblyName);
+                ContentPage p = (ContentPage)Activator.CreateInstance(t);
+                p.Title = file;
+                this.Children.Add(p);
+            }
+           
+            base.OnAppearing();
 
 
         }
