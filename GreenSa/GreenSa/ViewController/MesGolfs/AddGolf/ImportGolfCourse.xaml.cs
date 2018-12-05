f9493754ee1cabbf03460ff4523d79d6397863cd
Julien CHATEAU <julien.chateau@insa-rennes.fr>
2018-12-05 16:40:09 +0100

    change golf courses import view

--- GreenSa/GreenSa/ViewController/MesGolfs/AddGolf/ImportGolfCourse.xaml.cs
+++ GreenSa/GreenSa/ViewController/MesGolfs/AddGolf/ImportGolfCourse.xaml.cs
@@ -11,8 +11,9 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
 
-namespace GreenSa.ViewController.MesGolfs.AddGolf
+namespace GreenSa.ViewController.Option
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ImportGolfCourse : ContentPage
@@ -20,38 +21,13 @@ namespace GreenSa.ViewController.MesGolfs.AddGolf
         public ImportGolfCourse()
         {
             InitializeComponent();
-            format.Text = " < GolfCourse > \n<Name> Saint Jacques Parcours 9 trous </ Name >\n < NbTrous > 9 </ NbTrous >\n<NomGolf> Saint Jacques </ NomGolf >\n < Coordinates >\n < Trou >\n < par > 4 </ par >\n < lat > 48,070325 </ lat >\n < lng > -1,746956 </ lng >\n </ Trou >\n < Trou >\n < par > 4 </ par >\n < lat > 48,06957 </ lat >\n < lng > -1,744317 </ lng >\n </ Trou >\n....\n </ Coordinates >\n </ GolfCourse > ";
+            map.update();
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            String url = isPastebin.IsToggled?"https://pastebin.com/raw/"+urlText.Text:urlText.Text;
-            String res = "";
-            try
-            {
-                status.Text = "Récuperation en cours";
-
-                HttpClient client = new HttpClient();
-                 res = await client.GetStringAsync(url);
-                status.Text = "Texte récupéré, transformation en parcours de golf";
-            }
-            catch (Exception ex)
-            {
-                status.Text = "URL invalide";
-                return;
-            }
-            GolfCourse gc;
-            try
-            {
-                 gc = GolfXMLReader.getSingleGolfCourseFromText(res);
-                status.Text = "Transformé en parcours de golf, insertion dans la base de données";
-            }
-            catch (Exception ex)
-            {
-                status.Text = "Format de texte non valide";
-                return;
-            }
-            SQLite.SQLiteConnection connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+            
+            /*SQLite.SQLiteConnection connection = DependencyService.Get<ISQLiteDb>().GetConnection();
 
             try
             {
@@ -67,7 +43,7 @@ namespace GreenSa.ViewController.MesGolfs.AddGolf
             {
                 status.Text = "Problème dans l'insertion dans la base de données (duplication,...)";
                 connection.Rollback();
-            }
+            }*/
 
 
         }
